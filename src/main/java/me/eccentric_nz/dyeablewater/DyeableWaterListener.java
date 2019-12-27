7a3ff3022f05a2e65c2a7268ca7c239b25fa42aa
eccentricdevotion <eccentricdevotion@gmail.com>
2019-12-27 13:11:01 +1300

    Add dyeable water event sounds / don't allow water bucket placement on dyed cauldrons

--- src/main/java/me/eccentric_nz/dyeablewater/DyeableWaterListener.java
+++ src/main/java/me/eccentric_nz/dyeablewater/DyeableWaterListener.java
@@ -2,6 +2,7 @@ package me.eccentric_nz.dyeablewater;
 
 import org.bukkit.Color;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.block.Block;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.block.data.Levelled;
@@ -84,14 +85,17 @@ public class DyeableWaterListener implements Listener {
                             // get the model - could be 0 -> 150 in multiples of 10
                             int model = im.getCustomModelData() - 10000000;
                             BlockData stem = null;
+                            Sound sound = Sound.ITEM_BUCKET_EMPTY;
                             if (is.getType().equals(Material.WATER_BUCKET)) {
                                 // fill to level 3
                                 stem = plugin.getServer().createBlockData(DyeableWaterBlockData.MODEL_TO_DATA.get(model + 3));
                             } else if (is.getType().equals(Material.POTION) && cauldron.getLevel() == 0) {
                                 // fill to level 1
                                 stem = plugin.getServer().createBlockData(DyeableWaterBlockData.MODEL_TO_DATA.get(model + 1));
+                                sound = Sound.ITEM_BOTTLE_EMPTY;
                             }
-                            if (stem != null) {
+                            if (stem != null ) {
+                                player.playSound(player.getLocation(), sound, 1.0F, 1.0F);
                                 BlockData finalStem = stem;
                                 plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, () -> {
                                     block.setBlockData(finalStem);
@@ -109,9 +113,14 @@ public class DyeableWaterListener implements Listener {
                     int base = model - currentLevel;
                     if (armour.contains(is.getType())) {
                         if (model != null) {
+                            if (is.getType().equals(Material.WATER_BUCKET)) {
+                                event.setCancelled(true);
+                                return;
+                            }
                             BlockData stem;
                             Color color = getColor(base);
                             if (color != null) {
+                                player.playSound(player.getLocation(), "cauldron.dye", 1.0F, 1.0F);
                                 // dye the armour
                                 LeatherArmorMeta lam = (LeatherArmorMeta) is.getItemMeta();
                                 lam.setColor(color);
@@ -139,6 +148,7 @@ public class DyeableWaterListener implements Listener {
                         }
                         block.setBlockData(stem);
                         // change the glass bottle to a dyed potion
+                        player.playSound(player.getLocation(), Sound.ITEM_BOTTLE_FILL, 1.0F, 1.0F);
                         ItemStack potion = new ItemStack(Material.POTION);
                         ItemMeta potionMeta = potion.getItemMeta();
                         potionMeta.setDisplayName(getColorName(base) + " Dyed Water");
@@ -158,6 +168,7 @@ public class DyeableWaterListener implements Listener {
                         BlockData cauldronBlockData = Material.CAULDRON.createBlockData();
                         block.setBlockData(cauldronBlockData);
                         plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, () -> {
+                            player.playSound(player.getLocation(), Sound.ITEM_BUCKET_FILL, 1.0F, 1.0F);
                             // change the bucket to a dyed water bucket
                             ItemStack waterBucket = new ItemStack(Material.WATER_BUCKET);
                             ItemMeta bucketMeta = waterBucket.getItemMeta();
@@ -176,6 +187,7 @@ public class DyeableWaterListener implements Listener {
                             block.setBlockData(stem);
                             // change the potion to a glass bottle
                             ItemStack glassBottle = new ItemStack(Material.GLASS_BOTTLE);
+                            player.playSound(player.getLocation(), Sound.ITEM_BOTTLE_EMPTY, 1.0F, 1.0F);
                             player.getInventory().setItemInMainHand(glassBottle);
                             player.updateInventory();
                         }
